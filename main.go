b613fe8154649c037ee587db02df0ca2de08495e
chazu <chazbone@gmail.com>
2024-07-04 20:04:33 -0400

    add some more tests

--- main.go
+++ main.go
@@ -105,9 +105,14 @@ func main() {
 		"V:[TestView]",
 		"V:[TestView]-[TestTwo]",
 		"V:[TestView]-50-[TestTwo]",
+		"|[Test]|",
+		"|[Test][TestTwo]|",
+		"|[Test]-[TestTwo]|",
+		"|[Test]-(50)-[TestTwo]|",
 		"|-[Test]-|",
 		"|-50-[Test]-|",
-		"|-(50)-[Test]-(50)-|",
+		"|-50-[Test]-50-|",
+		"|-(>=50@10)-[Test]-(>=50@10)-|",
 	}
 	for _, c := range cases {
 		fmt.Printf("%s...", c)
